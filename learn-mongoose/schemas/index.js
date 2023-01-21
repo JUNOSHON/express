57d6b5b8cc32c3e0d1043afe5f2d7a4639b9c637
junoshon <sanoldfox@naver.com>
2023-01-21 16:30:03 +0900

    몽구스 사용하기

---
+++ learn-mongoose/schemas/index.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const connect = () => {
+
+    if(process.env.NODE_ENV !== 'production'){
+        mongoose.set('debug',true);
+    }
+
+    mongoose.connect('mongodb://junoshon:thswnsgh0317@localhost:27017/admin',{
+        dbName: 'nodejs',
+        useNewUrlParser: true,
+    }, (error) => {
+        if(error){
+            console.log('몽고디비 연결 에러',error);
+        }else{
+            console.log('몽고디빙 연결 성공');
+        }
+    });
+};
+
+mongoose.connection.on('error',(error) => {
+    console.error('몽고디비 연결 에러',error);
+});
+mongoose.connection.on('disconnected',()=>{
+    console.error('몽고디비 연결이 끊김. 연결 재시도.');
+    connect();
+});
+
+export default connect;
